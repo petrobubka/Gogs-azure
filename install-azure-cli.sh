08b04d70f4036b2bcadbc673ce50d872437b9627
Valentyn Zamykhivskyi <76389693+petrobubka@users.noreply.github.com>
2023-10-29 22:35:47 +0200

    Update install-azure-cli.sh

--- install-azure-cli.sh
+++ install-azure-cli.sh
@@ -6,4 +6,4 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
 sudo dnf install -y https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm
 
-sudo dnf install azure-cli
+sudo dnf install -y azure-cli
