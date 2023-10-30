951f43a5c28a293a45c33fc4dd38ccc4b8e80689
Valentyn Zamykhivskyi <76389693+petrobubka@users.noreply.github.com>
2023-10-30 09:44:44 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,8 +4,5 @@ terraform {
     storage_account_name = "gogsstorage"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    client_id            = var.ARM_CLIENT_ID
-    client_secret        = var.ARM_CLIENT_SECRET
-    tenant_id            = var.ARM_TENANT_ID
   }
 }
